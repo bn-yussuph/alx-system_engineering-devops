71e30e8d16d1fcf36912c7314ec85ab077868284
bn-yussuph <sulehajjaajj@gmail.com>
2025-01-11 02:11:28 +0000

    corrected task to to install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Installs flask using pip3
 
-package { 'flask':
-  command => 'pip3 install flask',
-  ensure   => '2.1.0'
+package { 'install flask':
+  command => 'pip3 install flask==2.1.0',
+  path => '/ussr/bin/',
+  unless => 'pip3 list | grep flask',
 }
