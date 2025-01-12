4e239e07be31d5918c02df7263e0ae331e48815f
bn-yussuph <sulehajjaajj@gmail.com>
2025-01-12 01:20:55 +0000

    attempted all ssh tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# set up a client SSH configuration
+
+file_line { 'Configure identityfle':
+	ensure => present,
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { Configure password authentication to no':
+	ensure => present,
+	path => '/etc/ssh/ssh_config',
+	line => 'PasswordAuthentication no',
+}
